f3bd8d29e10f4448c0480ce87f5054d88b9408c0
Naghal <anthony.duhaime9@outlook.com>
2025-09-23 09:29:05 -0400

    Add slug methods to Application enum

--- src/Enums/Application.php
+++ src/Enums/Application.php
@@ -14,4 +14,28 @@ enum Application: int
     case ARMP = 7;
     case ARMSMS = 8;
     case VIRTUAL_AGENT_MANAGER = 9;
+
+    public function slug(): string
+    {
+        return match($this) {
+            self::CUSTOMER_CENTRE       => 'customer_centre',
+            self::DEBTOR_CENTRE         => 'debtor_centre',
+            self::DASHBOARD             => 'dashboard',
+            self::DASHBOARD_A25         => 'dashboard_a25',
+            self::WALKIN                => 'walkin',
+            self::ARMDESK               => 'armdesk',
+            self::MDCV                  => 'mdcv',
+            self::ARMP                  => 'armp',
+            self::ARMSMS                => 'armsms',
+            self::VIRTUAL_AGENT_MANAGER => 'virtual_agent_manager',
+        };
+    }
+
+    public static function slugs(): array
+    {
+        return array_map(
+            fn (self $case) => $case->slug(),
+            self::cases()
+        );
+    }
 }
